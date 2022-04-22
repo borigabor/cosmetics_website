7b4126ba46afc3ae79e0009c72738e1419f0f5f2
unknown <borigabor08@gmail.com>
2022-04-22 09:35:04 +0200

    add file

---
+++ js/app.js
@@ -0,0 +1,110 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.add('active');
+};
+
+document.querySelector('#close-navbar').onclick = () => {
+  navbar.classList.remove('active');
+};
+
+let slide = document.querySelectorAll('.home .slide');
+let index = 0;
+
+function next(){
+  slide[index].classList.remove('active');
+  index = (index + 1) %  slide.length;
+  slide[index].classList.add('active');
+}
+
+function prev() {
+  slide[index].classList.remove('active');
+  index = (index - 1 + slide.length) % slide.length;
+  slide[index].classList.add('active');
+}
+
+var swiper = new Swiper(".products-slider", {
+  loop: true,
+  grabCursor : true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+    850: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  grabCursor : true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".arrivals-slider", {
+  loop: true,
+  grabCursor : true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    550: {
+      slidesPerView: 2,
+    },
+    850: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  loop: true,
+  grabCursor : true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    650: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
